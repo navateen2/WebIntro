5c2fc070becc96687da41c2c2923d287c63d1404
navaneet <navateen2@gmail.com>
2026-05-21 17:38:31 +0530

    fix border

--- js/test.js
+++ js/test.js
@@ -51,22 +51,97 @@ const employees = [
 ];
 
 
-console.log(employees.map((item)=>item.name));
-console.log(employees.filter((item)=>(item.age>27)).map((a)=>a.name));
-console.log(employees.filter((item)=>(item.department=="Engineering")).map((a)=>a.name));
-let total=employees.map((a)=>a.salary).reduce((a,b)=>(a+b));
-console.log(total);
-console.log(total/employees.length);
+// console.log(employees.map((item)=>item.name));
+// console.log(employees.filter((item)=>(item.age>27)).map((a)=>a.name));
+// console.log(employees.filter((item)=>(item.department=="Engineering")).map((a)=>a.name));
+// let total=employees.map((a)=>a.salary).reduce((a,b)=>(a+b));
+// console.log(total);
+// console.log(total/employees.length);
 
-let accumulator={};
+// let accumulator={};
 
-console.log(employees.reduce((a,b)=>{
-    if(b.department in a){
-        a[b.department].push(b.name);
-    }
-    else{
-        a[b.department]=[b.name];
-    }
-    return a;
+// console.log(employees.reduce((a,b)=>{
+//     if(b.department in a){
+//         a[b.department].push(b.name);
+//     }
+//     else{
+//         a[b.department]=[b.name];
+//     }
+//     return a;
 
-},{}));
+// },{}));
+
+// const car={
+//     brand: "Toyota",
+//     model: "Camry",
+//     year: 2024,
+// };
+
+// Object.freeze(car);
+// car.year = 2026;
+// car.color = "red";
+// console.log(car.year , car.color);
+
+
+
+// const createCounter=()=>{
+    
+//     let count=0;
+//     return ()=>{
+//         count++;    
+//         console.log(count);
+//     }    ;
+// };
+
+
+
+// const counter = createCounter();
+// counter();
+// counter();
+// counter();
+
+// function calculate(a,b,callback){
+//     console.log(callback(a,b));
+// }
+
+// calculate(10,5,(a,b)=>a+b);
+// calculate(10,5,(a,b)=>a-b);
+// calculate(10,5,(a,b)=>a*b);
+// const users = [{ name: "Alex" }, { name: "Sam" }];
+
+// const copy = structuredClone(users);
+
+// copy[0].name = "John";
+
+// console.log(users[0].name);
+
+
+
+// const state = {
+//   user: {
+//     name: "Alex",
+//     address: {
+//       city: "Kochi"
+//     }
+//   }
+// };
+
+// let {user}={...state};
+// let {name,address}=user;
+// let {city}={...address};
+// console.log(address);
+// console.log(city);
+// city="Banglore";
+// address={city:city};
+// user={name:name,address:address};
+// state2={user:user};
+
+// console.log(state);
+// console.log(state2);
+
+
+function add(a){
+    return (b)=>a+b;
+}
+
+console.log(add(1)(2));
